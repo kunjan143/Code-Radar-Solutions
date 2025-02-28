8f09ead05927088f3f92d1313dc88210cd0a6d1b
kunjan143 <kunjan1214.becse24@chitkara.edu.in>
2025-02-28 13:49:52 +0530

    Solution to Grade Evaluation

---
+++ Grade Evaluation@@6788dd63490637cfc44e1bab/code.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+int main()
+{
+    int ch;
+    scanf("%c" ,&ch);
+    if(ch==A)
+    {
+        printf("Excellent");
+    }
+    else if(ch==B)
+    {
+        printf("Good");
+    }
+    else if(ch==C)
+    {
+        printf("Average");
+    }
+    else if(ch==D)
+    {
+        printf("Below Average");
+    }
+    else if(ch==F)
+    {
+        printf("Fail");
+    }
+    else
+    {
+        printf("Invalid grade");
+    }
+}
