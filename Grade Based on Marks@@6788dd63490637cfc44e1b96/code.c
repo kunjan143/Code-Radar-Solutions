5ec3305cef982cd40fefa262e1d30e93e10f6fb5
kunjan143 <kunjan1214.becse24@chitkara.edu.in>
2025-02-28 11:24:16 +0530

    Solution to Grade Based on Marks

--- Grade Based on Marks@@6788dd63490637cfc44e1b96/code.c
+++ Grade Based on Marks@@6788dd63490637cfc44e1b96/code.c
@@ -10,15 +10,15 @@ int main()
     {
         printf("A");
     }
-    else if(per>=80 && per<=90)
+    else if(per>=80 && per<90)
     {
         printf("B");
     }
-    else if (per>=70 && per<=80)
+    else if (per>=70 && per<80)
     {
         printf("C");
     }
-    else if(per>=60 && per<=70)
+    else if(per>=60 && per<70)
     {
         printf("D");
     }
