5a1939daad4ab9bc17332b68986ec884d074a269
kunjan143 <kunjan1214.becse24@chitkara.edu.in>
2025-02-28 10:43:10 +0530

    Solution to Check Triangle Validity

--- Check Triangle Validity@@6788dd63490637cfc44e1b93/code.c
+++ Check Triangle Validity@@6788dd63490637cfc44e1b93/code.c
@@ -3,7 +3,7 @@ int main()
 {
     int angle1,angle2,angle3;
     scanf("%d%d%d", &angle1, &angle2, &angle3);
-    sum = angle1+angle2+angle3;
+    sum = angle1 + angle2 + angle3;
     if(sum==180 && angle1 > 0 && angle2 > 0 && angle3 > 0)
     {
         printf("Valid");
