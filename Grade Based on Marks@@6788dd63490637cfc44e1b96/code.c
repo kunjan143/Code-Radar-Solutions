f469b5dde170b788bc826d690f578a7fa6ca3779
kunjan143 <kunjan1214.becse24@chitkara.edu.in>
2025-02-28 16:20:21 +0530

    Solution to Grade Based on Marks

--- Grade Based on Marks@@6788dd63490637cfc44e1b96/code.c
+++ Grade Based on Marks@@6788dd63490637cfc44e1b96/code.c
@@ -1,24 +1,21 @@
 #include<stdio.h>
 int main()
 {
-    int a,b,c,d,f;
-    float per;
-    scanf("%f %f %f %f %f", &a, &b , &c ,&d ,&f);
-    per = (a+b+c+d+f)/5.0;
-    printf("percentage=%.2f",per);
-    if(per>=90)
+    int a;
+    scanf("%d", &a);
+    if(a>=90)
     {
         printf("A");
     }
-    else if(per>=80 && per<90)
+    else if(a>=80 && a<90)
     {
         printf("B");
     }
-    else if (per>=70 && per<80)
+    else if(a>=70 && a<80)
     {
         printf("C");
     }
-    else if(per>=60 && per<70)
+    else if(a>=60 && a<70)
     {
         printf("D");
     }
