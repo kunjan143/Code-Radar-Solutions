0ec161a7876a85a5290c833837f75c32d25e1971
kunjan143 <kunjan1214.becse24@chitkara.edu.in>
2025-02-28 13:23:16 +0530

    Solution to Find Day of the Week

---
+++ Find Day of the Week@@6788dd63490637cfc44e1ba7/code.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int a;
+    scanf("%d" ,&a);
+    if(a<=1 && a>=7)
+    {
+        printf("name of the day");
+    }
+    else
+    {
+        printf("Invalid");
+    }
+}
